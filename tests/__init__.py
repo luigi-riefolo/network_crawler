376fb5860c573416ac71a0dfe5437011858398b6
Luigi Riefolo <luigi.riefolo@gmail.com>
2016-11-08 01:40:52 +0100

    Update __init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -9,7 +9,7 @@ try:
     from selenium import webdriver
     from selenium.common.exceptions import WebDriverException
 except ImportError as imp_err:
-    raise ImportError('Failed to import \'selenium\':\n' + imp_err)
+    raise ImportError('Failed to import \'selenium\':\n' + str(imp_err))
 
 from network_crawler.api.operator_web_site import OperatorWebSite
 
